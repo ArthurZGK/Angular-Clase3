b86d2908e9104282dca93a05993ded4a1fb02ecd
ArthurZGK <jarturopg_25@hotmail.com>
2021-02-18 21:56:19 -0500

    pipe tarea terminado

--- src/app/domain/pipes/to-format-phone.pipe.ts
+++ src/app/domain/pipes/to-format-phone.pipe.ts
@@ -1,12 +1,14 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { concat } from 'rxjs';
 
 @Pipe({
   name: 'toFormatPhone'
 })
 export class ToFormatPhonePipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: string): string {
+    const telefono:string="000000000".concat(value).substr(-9,9);
+    return telefono.substring(0,1).concat("-",telefono.substring(1,5),"-",telefono.substring(5))
   }
 
 }
--- src/app/domain/views/login/login.component.ts
+++ src/app/domain/views/login/login.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 // title:string='Login Clase3';
 rompeTexto:string="Hola soy un texto a romper abcdeeeee ccccccc";
+miNumero:string="992115685";
 // hoy:Date=new Date();
   constructor() { }
 
